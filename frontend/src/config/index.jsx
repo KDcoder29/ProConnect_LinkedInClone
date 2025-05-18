4252b36f74d01300dc925b5c27c5403fbe8a2917
Kandarp Vaghela <kandarp0629@gmail.com>
2025-05-18 14:04:37 +0530

    Backend Deployed URL Updated

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,7 +1,7 @@
 import { default as axios } from "axios";
 
 
-export const BASE_URL = "http://localhost:9080"
+export const BASE_URL = "https://proconnect-sh7q.onrender.com"
 const clientServer = axios.create({
     baseURL : BASE_URL,
 })
