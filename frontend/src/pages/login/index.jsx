4f1b2b4191da890b2c32376cdc5b2054c09cff75
Kandarp Vaghela <kandarp0629@gmail.com>
2025-05-31 01:20:12 +0530

    Version 1.0.1 Hydration error Check and solved for using Eye icon. UPDATED password hide show option

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,95 +1,111 @@
-import UserLayout from '@/layout/userlayout'
-import { useRouter } from 'next/router'
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import styles from "./style.module.css";
-import { loginUser, registerUser } from '@/config/redux/action/authAction'
+import UserLayout from '@/layout/userlayout';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styles from './style.module.css';
+import { loginUser, registerUser } from '@/config/redux/action/authAction';
 import { emptyMessage } from '@/config/redux/reducer/authReducer';
+import dynamic from 'next/dynamic';
+
+// Dynamically import icons (client-side only to avoid hydration issues)
+const FaEye = dynamic(() => import('react-icons/fa').then(mod => mod.FaEye), { ssr: false });
+const FaEyeSlash = dynamic(() => import('react-icons/fa').then(mod => mod.FaEyeSlash), { ssr: false });
 
 function LoginComponent() {
-  
-  const authState = useSelector((state)=> state.auth)
+  const authState = useSelector((state) => state.auth);
   const router = useRouter();
-  const [userLoginMethod,setUserLoginMethod] = useState(false);
   const dispatch = useDispatch();
 
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
-  const [username,setUsername] = useState("");
-  const [name,setName] = useState("");
+  const [userLoginMethod, setUserLoginMethod] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [name, setName] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
   useEffect(() => {
-    if(authState.loggedIn){
-      router.push("/dashboard")
+    if (authState.loggedIn) {
+      router.push('/dashboard');
     }
-  },[authState.loggedIn])
-  
-  useEffect(()=>{
-    dispatch(emptyMessage());
+  }, [authState.loggedIn]);
 
-  },[userLoginMethod])
-  useEffect(()=>{
-    if(localStorage.getItem("token")){
-      router.push("/dashboard");
+  useEffect(() => {
+    dispatch(emptyMessage());
+  }, [userLoginMethod]);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && localStorage.getItem('token')) {
+      router.push('/dashboard');
     }
-  })
+  }, []);
+
   const handleRegister = () => {
-    console.log("registering... ");
-    dispatch( registerUser({username,password,email,name}) );
+    dispatch(registerUser({ username, password, email, name }));
     setUserLoginMethod(true);
   };
-  const handleLogin= () =>{
-    console.log("Login...");
-    dispatch(loginUser({email,password}))
+
+  const handleLogin = () => {
+    dispatch(loginUser({ email, password }));
   };
 
   return (
     <UserLayout>
       <div className={styles.container}>
         <div className={styles.cardContainer}>
-        <div className={styles.cardContainer_left}>
-          <p className={styles.cardleft_heading}>{userLoginMethod ? "Sign in" : "Sign Up"}</p>
-          <p style={{color : authState.isError ? "red" : "green"}}>{authState.message.message}</p>
+          <div className={styles.cardContainer_left}>
+            <p className={styles.cardleft_heading}>{userLoginMethod ? 'Sign in' : 'Sign Up'}</p>
+            <p style={{ color: authState.isError ? 'red' : 'green' }}>{authState.message.message}</p>
 
-          <div className={styles.inputContainers}>
-            
-              {!userLoginMethod && (<div className={styles.inputRow}> 
-          <input onChange={(e)=> setUsername(e.target.value)} className={styles.inputfield} type="text" placeholder='Username' />
-          <input onChange={(e)=> setName(e.target.value)} className={styles.inputfield} type="text" placeholder='Name' /> 
-            </div>)}
+            <div className={styles.inputContainers}>
+              {!userLoginMethod && (
+                <div className={styles.inputRow}>
+                  <input onChange={(e) => setUsername(e.target.value)} className={styles.inputfield} type="text" placeholder="Username" />
+                  <input onChange={(e) => setName(e.target.value)} className={styles.inputfield} type="text" placeholder="Name" />
+                </div>
+              )}
 
-            <input onChange={(e)=> setEmail(e.target.value)} className={styles.inputfield} type="text" placeholder='Email' />
-            <input onChange={(e)=> setPassword(e.target.value)} className={styles.inputfield} type="text" placeholder='Password' />
+              <input onChange={(e) => setEmail(e.target.value)} className={styles.inputfield} type="text" placeholder="Email" />
 
-            <div onClick={()=>{
-              if(userLoginMethod){
-                handleLogin();
-              }else{
-                handleRegister();
-              }
-            }} className={styles.buttonWithOutline}>
-              <p>{userLoginMethod ? "Sign in" : "Sign Up"}</p>
-            </div>
-
-
-            </div>  
-        
-        </div>
-        <div className={styles.cardContainer_right}>
-         
-            {userLoginMethod ? <p>Don't Have an Account?</p> : <p>Already Have an Account?</p>}
-          
-          <div onClick={()=>{
-            setUserLoginMethod(!userLoginMethod);
-          }} style ={{color: "black", textAlign: "center"}} className={styles.buttonWithOutline}>
-              <p>{userLoginMethod ? "Sign Up" : "Sign in"}</p>
-            </div>
+              {/* Password Field with Eye Toggle */}
+              <div className={styles.passwordFieldWrapper}>
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  className={styles.inputfield}
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder="Password"
+                />
+                <span className={styles.eyeIcon} onClick={() => setShowPassword((prev) => !prev)}>
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </span>
               </div>
+
+              <div
+                onClick={() => {
+                  userLoginMethod ? handleLogin() : handleRegister();
+                }}
+                className={styles.buttonWithOutline}
+              >
+                <p>{userLoginMethod ? 'Sign in' : 'Sign Up'}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.cardContainer_right}>
+            {userLoginMethod ? <p>Don't Have an Account?</p> : <p>Already Have an Account?</p>}
+            <div
+              onClick={() => {
+                setUserLoginMethod(!userLoginMethod);
+              }}
+              style={{ color: 'black', textAlign: 'center' }}
+              className={styles.buttonWithOutline}
+            >
+              <p>{userLoginMethod ? 'Sign Up' : 'Sign in'}</p>
+            </div>
+          </div>
         </div>
       </div>
-
     </UserLayout>
-  )
+  );
 }
- 
-export default LoginComponent
+
+export default LoginComponent;
